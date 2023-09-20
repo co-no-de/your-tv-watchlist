6afd1263fba549d1262da6c1abaad5030fff049e
Erik <erik@web-code.nl>
2023-09-20 09:04:03 +0200

    Voor start responsive

--- StackNavigator.js
+++ StackNavigator.js
@@ -2,6 +2,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen, SingleScreen, AllSeriesScreen, SearchScreen, WatchListScreen } from './screens'
+import { Ionicons } from "@expo/vector-icons";
+import { colors } from './assets/data'
 
 const StackNavigator = () => {
   const Tab = createBottomTabNavigator();
@@ -9,7 +11,40 @@ const StackNavigator = () => {
 
   function BottomTabs() {
     return (
-      <Tab.Navigator>
+      <Tab.Navigator   screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: {
+          width: "100%",
+          alignItems: "center",
+        },
+      
+        tabBarIcon: ({ focused }) => {
+          let iconName;
+         
+
+          if (route.name === "Home" ) {
+            iconName = focused ? "ios-home-sharp" : "ios-home-outline";
+         
+          } else if (route.name === "All Series") {
+            iconName = focused ? "file-tray-full" : "file-tray-full-outline";
+         
+          } else if (route.name === "Search") {
+            iconName = focused ? "search-circle" : "search-circle-outline";
+         
+          } else if (route.name === "Watchlist") {
+            iconName = focused
+              ? "list-circle-sharp"
+              : "list-circle-outline";
+         
+          }
+
+          return <Ionicons
+          name={iconName}
+          size={24}
+          color={colors.gradientEnd}
+        />
+        },
+      })}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="All Series" component={AllSeriesScreen} />
         <Tab.Screen name="Search" component={SearchScreen} />
@@ -20,7 +55,9 @@ const StackNavigator = () => {
 
   return (
     <NavigationContainer>
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{
+       headerShown: false,
+    }}>
       <Stack.Screen name="Main" component={BottomTabs} />
       <Stack.Screen name="SingleScreen" component={SingleScreen} />
     </Stack.Navigator>
